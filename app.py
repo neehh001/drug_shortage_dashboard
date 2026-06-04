4ca062c478c0c0f9ce8f47f5ac41e28ffa41c884
Neha Khadeeja <khadeejaneha880@gmail.com>
2026-06-04 12:45:05 +0530

    add saved model

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import sqlite3
 import plotly.express as px
 import plotly.graph_objects as go
 
+import pickle
+
+# Load model
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
 # --- Page config ---
 st.set_page_config(
     page_title="Drug Shortage Early Warning System",
